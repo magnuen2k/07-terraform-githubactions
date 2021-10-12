dac8deb762c989af88f0acaa8dd05897ecb6fcbe
EC2 Default User <ec2-user@ip-172-31-1-126.eu-north-1.compute.internal>
2021-10-12 08:22:58 +0000

    huge update

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          jeg er best
+          jeg er best v2
         </p>
         <a
           className="App-link"
